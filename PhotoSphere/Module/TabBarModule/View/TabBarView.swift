c2860940d07e321e25e6880dd9d93c550e6a9c0d
Sorokin Dmitriy <dmitrijsorokin532@gmail.com>
2024-04-22 12:00:15 -0700

    Добавил камеру

--- PhotoSphere/Module/TabBarModule/View/TabBarView.swift
+++ PhotoSphere/Module/TabBarModule/View/TabBarView.swift
@@ -24,7 +24,7 @@ class TabBarView: UITabBarController {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(hideTabBar), name: .hideTabBar, object: nil)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(goToMainView), name: .goToMain, object: nil)
          tabs.enumerated().forEach {
             let offsets: [Double] = [-100, 0, 100]
             let tabButton = createTabBarButton(icon: $0.element, tag: $0.offset, offsetX: offsets[$0.offset], isBigButton: $0.offset == 1 ? true : false)
@@ -33,6 +33,11 @@ class TabBarView: UITabBarController {
         }
         
         view.addSubview(tabBarView)
+    }
+    
+    @objc func goToMainView(){
+        self.selectedIndex = 0
+        
     }
     
     @objc func hideTabBar(sender: Notification) {
